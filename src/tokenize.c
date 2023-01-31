ce5d4d4bd62709256fcd847316db78e54900437d
Stuart Rapoport <stuartrapop@gmail.com>
2023-01-31 16:32:36 +0100

    treat quotes

--- src/tokenize.c
+++ src/tokenize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenize.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pmarquis <astrorigin@protonmail.com>       +#+  +:+       +#+        */
+/*   By: srapopor <srapopor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 02:17:03 by pmarquis          #+#    #+#             */
-/*   Updated: 2023/01/30 14:35:57 by pmarquis         ###   lausanne.ch       */
+/*   Updated: 2023/01/31 16:28:35 by srapopor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,15 @@ char	*tokenize(const char *s, t_token *tok)
 {
 	static const t_tokspec	tokspecs[] = {
 		{"&&", 2, tok_and, 0},
-		{"<<", 2, tok_outappend, &tokenize_var},
-		{">>", 2, tok_inheredoc, &tokenize_var},
+		{"<<", 2, tok_outappend, 0},
+		{">>", 2, tok_inheredoc, 0},
 		{"||", 2, tok_or, 0},
-		{"\"", 1, tok_dquote, &tokenize_var},
-		{"\'", 1, tok_squote, &tokenize_var},
 		{"(", 1, tok_lparen, 0},
 		{")", 1, tok_rparen, 0},
-		{"<", 1, tok_outfile, &tokenize_var},
-		{">", 1, tok_infile, &tokenize_var},
+		{"<", 1, tok_outfile, 0},
+		{">", 1, tok_infile, 0},
 		{"|", 1, tok_pipe, 0},
-		{0, 0, 0, 0}
+		{0, 0, tok_string, 0}
 	};
 	t_tokspec	*spec;
 
--- src/tokenize_var.c
+++ src/tokenize_var.c
@@ -3,23 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   tokenize_var.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pmarquis <astrorigin@protonmail.com>       +#+  +:+       +#+        */
+/*   By: srapopor <srapopor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 19:04:17 by pmarquis          #+#    #+#             */
-/*   Updated: 2023/01/18 00:51:53 by pmarquis         ###   lausanne.ch       */
+/*   Updated: 2023/01/31 16:06:56 by srapopor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+typedef struct s_parse {
+	int	count;
+	int	index;
+	int	within_piece;
+	int	within_single_quotes;
+	int	within_double_quotes;
+}	t_parse;
+
+int	is_sep(char c, t_parse parse)
+{
+	if (ft_strchr(" |<>()&", c) \
+	&& (!parse.within_single_quotes && !parse.within_double_quotes))
+		return (1);
+	return (0);
+}
+
+int	treat_quotes(char c, t_parse *parse)
+{
+	if (c == '\'' && !parse->within_double_quotes) 
+	{
+		parse->within_single_quotes = !parse->within_single_quotes;
+		return (1);
+	}
+	if (c == '\"' && !parse->within_single_quotes) 
+	{
+		parse->within_double_quotes = !parse->within_double_quotes;
+		return (1);
+	}
+	return (0);
+}
+
 static char	*_tokenize_var(const char *s, t_token *tok)
 {
 	const char	*start = s;
+	t_parse 	parse;
 
-	/* printf("_tokenize_var=%s\n", s); */
-	while (*s && !ft_isspace(*s))
+	ft_memset(&parse, 0, sizeof(parse));
+	// printf("_tokenize_var=%s\n", s);
+	while (*s && !is_sep(*s, parse))
+	{
+		treat_quotes(*s, &parse);
 		++s;
-	tok->data = ft_strndup(start, s - start + 1);
+	}
+	tok->data = ft_strndup(start, s - start );
+	printf("value:%s\n", tok->data);
 	if (!tok->data)
 	{
 		error("ft_strndup", "nomem");
@@ -36,9 +73,35 @@ char	*tokenize_var(const char *s, t_token *tok)
 		token_fini(tok);
 		return (0);
 	}
-	if (*s == '\'')
-		return (tokenize_squote(s + 1, tok));
-	else if (*s == '\"')
-		return (tokenize_dquote(s + 1, tok));
 	return (_tokenize_var(s, tok));
 }
+// static char	*_tokenize_var(const char *s, t_token *tok)
+// {
+// 	const char	*start = s;
+
+// 	// printf("_tokenize_var=%s\n", s); 
+// 	while (*s && !ft_isspace(*s))
+// 		++s;
+// 	tok->data = ft_strndup(start, s - start + 1);
+// 	if (!tok->data)
+// 	{
+// 		error("ft_strndup", "nomem");
+// 		return (0);
+// 	}
+// 	return ((char *) s);
+// }
+
+// char	*tokenize_var(const char *s, t_token *tok)
+// {
+// 	/* printf("tokenize_var=%s\n", s); */
+// 	if (!skip_spaces(&s))
+// 	{
+// 		token_fini(tok);
+// 		return (0);
+// 	}
+// 	if (*s == '\'')
+// 		return (tokenize_squote(s + 1, tok));
+// 	else if (*s == '\"')
+// 		return (tokenize_dquote(s + 1, tok));
+// 	return (_tokenize_var(s, tok));
+// }
