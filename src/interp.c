1ab1f2435ca860290c2a4899aad1b964a45c3ec0
Stanislas Marquis <stan@astrorigin.com>
2023-02-02 15:27:47 +0100

    cleaning

--- src/interp.c
+++ src/interp.c
@@ -6,7 +6,7 @@
 /*   By: srapopor <srapopor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 21:42:18 by pmarquis          #+#    #+#             */
-/*   Updated: 2023/01/31 16:30:07 by srapopor         ###   ########.fr       */
+/*   Updated: 2023/02/02 15:17:44 by pmarquis         ###   lausanne.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,17 @@ int	interp(const char *s, char *env[])
 	t_token	tok;
 	t_node	root;
 
-	/* printf("interp=%s\n", s); */
-	ft_memset(&tok, 0, sizeof(t_token));
+	/* printf("interp='%s'\n", s); */
 	ft_memset(&root, 0, sizeof(t_node));
-	while (1)
+	while (s && *s)
 	{
-		if (!skip_spaces(&s))
-			break ;
-		// printf("string %s\n", s);
-		// tokenize_var(s, &tok);
-		// return (1);
+		ft_memset(&tok, 0, sizeof(t_token));
 		s = tokenize(s, &tok);
-		// if (!s)
-		// 	return (error("interp", s) + token_fini(&tok) + node_fini(&root));
-		// if (!_parse(&tok, &root))
-		// 	return (token_fini(&tok) + node_fini(&root));
+		/* if (!_parse(&tok, &root)) */
+		/* 	return (token_fini(&tok) + node_fini(&root)); */
+		token_fini(&tok);
 	}
 	/* exec(&node, env); */
-	root_node_fini(&root);
-	token_fini(&tok);
+	/* root_node_fini(&root); */
 	return (1);
 }
--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: srapopor <srapopor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 08:46:58 by pmarquis          #+#    #+#             */
-/*   Updated: 2023/02/02 02:06:16 by pmarquis         ###   lausanne.ch       */
+/*   Updated: 2023/02/02 15:25:52 by pmarquis         ###   lausanne.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,22 @@
 
 # define PS1	">>> "
 
+/*
+ *	lexer
+ */
+
 typedef enum e_toktype
 {
 	tok_var = 0,
-	tok_lparen,
-	tok_rparen,
-	tok_and,
-	tok_or,
 	tok_infile,
 	tok_outfile,
 	tok_inheredoc,
 	tok_outappend,
 	tok_pipe,
-	tok_string
+	tok_lparen,
+	tok_rparen,
+	tok_and,
+	tok_or
 }	t_toktype;
 
 typedef struct s_token
@@ -47,6 +50,10 @@ typedef struct s_token
 	t_toktype	tp;
 	char		*data;
 }	t_token;
+
+/*
+ *	syntax tree
+ */
 
 typedef enum e_ndtype
 {
@@ -56,7 +63,9 @@ typedef enum e_ndtype
 }	t_ndtype;
 
 typedef struct s_node	t_node;
+
 typedef struct s_cmdline t_cmdline;
+
 struct s_node
 {
 	t_ndtype	tp;
@@ -65,6 +74,10 @@ struct s_node
 	t_node		*right;
 	t_cmdline	*cmdline;
 };
+
+/*
+ *	...
+ */
 
 typedef enum e_opt
 {
@@ -113,8 +126,6 @@ int		root_node_fini(t_node *nd);
 int		skip_spaces(const char **s);
 int		token_fini(t_token *tok);
 char	*tokenize(const char *s, t_token *tok);
-char	*tokenize_dquote(const char *s, t_token *tok);
-char	*tokenize_squote(const char *s, t_token *tok);
 char	*tokenize_var(const char *s, t_token *tok);
 
 #endif
