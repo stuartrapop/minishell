0dca38184312aae6bd4b2d2c6e3d4b7ce9dea1e0
Stanislas Marquis <stan@astrorigin.com>
2023-02-06 23:45:50 +0100

    demo ft_arr

--- libft/arr/tests/test.c
+++ libft/arr/tests/test.c
@@ -6,37 +6,65 @@
 /*   By: pmarquis <astrorigin@protonmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 15:50:31 by pmarquis          #+#    #+#             */
-/*   Updated: 2022/12/23 16:00:30 by pmarquis         ###   lausanne.ch       */
+/*   Updated: 2023/02/06 23:35:23 by pmarquis         ###   lausanne.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_arr.h"
 
-static void	_print(int *i)
+static void	_print(void *str)
 {
-	printf("%d\n", *i);
+	char	*s;
+
+	s = *(char **) str;
+	printf("got = %s\n", s);
 }
 
-int	main(void)
+int main(void)
 {
-	int		i;
+	char	*s;
 	t_arr	a;
 
-	ft_arr_init(&a, 1, sizeof(int));
+	ft_arr_init(&a, 0, sizeof(char *));
 
-	i = 42;
-	ft_arr_append(&a, &i, 0);
-	ft_arr_trav(&a, (t_fn1) &_print);
+	s = ft_strdup("words");
+	assert(s);
 
-	i = *(int *)ft_arr_get(&a, 0);
-	assert(i == 42);
+	ft_arr_append(&a, &s, 0);
+	ft_arr_trav(&a, (t_fn1) _print);
 
-	i = 52;
-	ft_arr_append(&a, &i, 0);
-	ft_arr_trav(&a, (t_fn1) &_print);
+	s = *(char **) ft_arr_get(&a, 0);
+	assert(s && !ft_strcmp(s, "words"));
 
-	ft_arr_fini(&a, 0);
+	ft_arr_fini(&a, &ft_del);
 
 	printf("Memusage = %zu\n", ft_memusage());
 	return (0);
 }
+
+
+/* static void	_print(int *i) */
+/* { */
+/* 	printf("%d\n", *i); */
+/* } */
+
+/* int	main(void) */
+/* { */
+/* 	int		i; */
+/* 	t_arr	a; */
+
+/* 	ft_arr_init(&a, 1, sizeof(int)); */
+
+/* 	i = 42; */
+/* 	ft_arr_append(&a, &i, 0); */
+/* 	ft_arr_trav(&a, (t_fn1) &_print); */
+
+/* 	i = 52; */
+/* 	ft_arr_append(&a, &i, 0); */
+/* 	ft_arr_trav(&a, (t_fn1) &_print); */
+
+/* 	ft_arr_fini(&a, 0); */
+
+/* 	printf("Memusage = %zu\n", ft_memusage()); */
+/* 	return (0); */
+/* } */
