3ca6278d53f16cec470900f2bb116a79f68dfa03
Stanislas Marquis <stan@astrorigin.com>
2023-02-12 18:05:54 +0100

    ps1

--- src/ps1.c
+++ src/ps1.c
@@ -6,7 +6,7 @@
 /*   By: pmarquis <astrorigin@protonmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 02:29:33 by pmarquis          #+#    #+#             */
-/*   Updated: 2023/02/12 02:50:34 by pmarquis         ###   lausanne.ch       */
+/*   Updated: 2023/02/12 18:03:15 by pmarquis         ###   lausanne.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 char	*ps1(void)
 {
-	char	*ps1;
+	char		*ps1;
+	static char	buf[32];
 
 	ps1 = ft_env_var("PS1", g_shell->env.data);
 	if (!ps1 || !*ps1 || ft_strisspace(ps1))
-		ps1 = PS1;
+	{
+		buf[0] = '(';
+		ft_itoa(g_shell->retval, &buf[1]);
+		ft_strcatchr(buf, ')', 1);
+		ft_strcpy(ft_strchr(buf, 0), PS1);
+		ps1 = buf;
+	}
 	return (ps1);
 }
