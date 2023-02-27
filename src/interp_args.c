2ea5cb428d237e3823d6db5fd0481a3710132d64
Stanislas Marquis <stan@astrorigin.com>
2023-02-27 14:39:36 +0100

    fix crash

--- src/interp_args.c
+++ src/interp_args.c
@@ -6,7 +6,7 @@
 /*   By: pmarquis <astrorigin@protonmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 04:27:41 by pmarquis          #+#    #+#             */
-/*   Updated: 2023/02/16 19:06:13 by pmarquis         ###   lausanne.ch       */
+/*   Updated: 2023/02/27 14:36:35 by pmarquis         ###   lausanne.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,16 @@
 
 int	interp_args(int argc, char *argv[])
 {
-	int	i;
+	int		i;
+	char	*p;
 
 	i = -1;
 	while (++i < argc)
-		interp(argv[i]);
+	{
+		if (!ft_strdup2(argv[i], &p))
+			enomem();
+		interp(p);
+	}
 	i = g_shell->retval;
 	ft_memtrash();
 	return (i);
